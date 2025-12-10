a93b384f31856a58470a945edddd8bc78b8953ad
thenguyenn <nguyenbathe2711@gmail.com>
2025-12-10 19:49:06 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export default function App() {
+
+  const info = [
+    { name: "Nguyễn Bá Thế", age: 21 , hobby: "Tập gym" , profestion: "Lập trình viên" },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {info.map((item, index) => (
+        <div key={index}>
+          <h1>Họ và tên: {item.name}</h1>
+          <h2>Tuổi: {item.age}</h2>
+          <h2>Sở thích: {item.hobby}</h2>
+          <h2>Nghề nghiệp: {item.profestion}</h2>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
